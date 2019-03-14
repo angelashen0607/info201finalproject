98b63838926bfa1a99419bc9d88a08c00009360f
Tanner Kooistra <tannerckooistra@gmail.com>
2019-03-13 19:11:32 -0700

    Added correlational data to popups

--- server.R
+++ server.R
@@ -44,17 +44,19 @@ server <- function(input, output) {
       addProviderTiles(providers$CartoDB.Positron) %>% 
       addCircleMarkers(
         lat = ~lat, 
-        lng =~long,
+        lng = ~long,
         popup = paste0(
           paste0("<img src = ", fifa_data$Photo, ">"),"<br>", 
           "Name: ", fifa_data$Name, "<br>",
           "Nationality: ", fifa_data$Nationality, "<br>",
           "Wage: ", fifa_data$Wage, "<br>",
           "Value: ", fifa_data$Value, "<br>",
-          "Club: ", fifa_data$Club, "<br>"
+          "Club: ", fifa_data$Club, "<br>",
+          input$color, ": ", fifa_data[, input$color]
         ),
-        color = input$color, 
-        radius = 5
+        color = "black", 
+        radius = 5,
+        stroke = FALSE
       )
   })
   filtered <- reactive({
